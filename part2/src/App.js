61c2574a3f1c47a0435b896d8a0aa47cdc5bd317
JohnKarima <karimajohn24@gmail.com>
2022-09-29 16:28:25 +0300

    2.4. Handling an arbitrary number of courses

--- part2/src/App.js
+++ part2/src/App.js
@@ -3,7 +3,7 @@ const Course = (props) => {
 }
 
 const Header = (props) => {
-    return <h1>{props.course}</h1>
+    return <h1>{props.courses}</h1>
 }
 
 const Content = (props) => {
@@ -19,33 +19,77 @@ const Part = (props) => {
 }
 
 const App = () => {
-    const course = {
-        id: 1,
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10,
-                id: 1,
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7,
-                id: 2,
-            },
-            {
-                name: 'State of a component',
-                exercises: 14,
-                id: 3,
-            },
-        ],
-    }
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1,
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2,
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3,
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4,
+                },
+            ],
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1,
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2,
+                },
+            ],
+        },
+    ]
+
+    var totalEx = courses[0].parts.reduce(function (sum, totals) {
+        return sum + totals.exercises
+    }, 0)
+
+    console.log(totalEx)
+
+    var totalEx2 = courses[1].parts.reduce(function (sum, totals) {
+        return sum + totals.exercises
+    }, 0)
+
+    console.log(totalEx2)
 
     return (
         <>
-            <Course course={course} />
-            <Header course={course.name} />
-            <Content parts={course.parts}/>
+            <Course courses={courses} />
+            <Header courses={courses[0].name} />
+            <Content parts={courses[0].parts} />
+            <div>
+                <b>Total of {totalEx} exercises</b>
+            </div>
+
+            <Header courses={courses[1].name} />
+            <Content parts={courses[1].parts} />
+            <div>
+                <b>Total of {totalEx2} exercises</b>
+            </div>
         </>
     )
 }
